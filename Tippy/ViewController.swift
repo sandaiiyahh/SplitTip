f5b9138ce433dad56dc02e330a48a0628810bc65
Sandy Dai <sandydai98@gmail.com>
2020-08-08 15:09:37 -0400

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Sandy Dai on 8/8/20.
+//  Copyright © 2020 Sandy Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
